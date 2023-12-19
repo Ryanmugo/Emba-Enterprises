8783a070ee9f3df621fd73f0cb3ca06a727f7ce8
Ryan Mugo <gathariryan@gmail.com>
2023-12-19 17:47:55 +0100

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,5 @@
+const SignUp = () => {
+  return <div>SignIn</div>;
+};
+
+export default SignUp;
