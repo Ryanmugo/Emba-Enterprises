37e8668f9e0c6200e860b926d40f634505ad5cf8
Ryan Mugo <gathariryan@gmail.com>
2023-12-23 23:07:47 +0100

    Created a search API for my search input to help users who lets say want tio search for something

--- server/controllers/listingControllers.js
+++ server/controllers/listingControllers.js
@@ -55,7 +55,7 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
-//Getting the listing!!
+//Getting the listing from the landlord if he has!!
 export const getListing = async (req, res, next) => {
   try {
     const listing = await Listing.findById(req.params.id);
@@ -63,6 +63,58 @@ export const getListing = async (req, res, next) => {
       return next(errorHandler(404, "Listing not found!"));
     }
     res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//Search API for the Search Bar to help lets say you want to look for houses!!
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 9;
+    let offer = req.query.offer; //This is for the offer
+
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished; //This is for the furnished;
+
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking; //This is for the parking!!
+
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type; //This is for type (Rent or Sale or even both)!!
+
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
